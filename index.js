25571d11817cd6ba3ea432f3ed5825de5b98d68d
Esteban Chávez <73208993+EstebanChavez27@users.noreply.github.com>
2021-07-14 20:10:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const API_KEY = '8395e1b9e77b72a04105a7c828a6b638'
+
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData(data))
+}
+
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity + "%",
+        pressure: data.main.pressure + "hPa",
+        temperature: data.main.temp + "ºC",
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+
+    cleanUp();
+}
+
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+}
+
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+}
+
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
